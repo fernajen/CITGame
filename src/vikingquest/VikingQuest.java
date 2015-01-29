e65a3d95c7c2734dd46a3b7613e1d16e0d63a32b
EstevezAmy <amynboren@gmail.com>
2015-01-29 14:31:52 -0600

    First Setup with Jennifer

---
+++ src/vikingquest/VikingQuest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vikingquest;
+
+import byui.cit260.vikingQuest.model.Game;
+
+/**
+ *
+ * @author Amy
+ */
+public class VikingQuest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Game gameOne = new Game();
+        
+        gameOne.setName("Fred Flinstone");
+        gameOne.setBestTime(7.00);
+        
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo);
+        
+        
+    }
+    
+}
