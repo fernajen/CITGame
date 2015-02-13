639b59bdc4e15672ec4fe0f48b80603b077f11df
EstevezAmy <amynboren@gmail.com>
2015-02-13 12:08:33 -0600

    Control Layer Class calculations for addItem and calculateCurrentScore. The build was successful and ran without errors.

--- src/byui/cit260/vikingQuest/control/addItem.java
+++ src/byui/cit260/vikingQuest/control/addItem.java
@@ -10,5 +10,24 @@ package byui.cit260.vikingQuest.control;
  * @author Amy
  */
 public class addItem {
-    
+    public static void main(String args[]){
+        
+        //Declare Variables
+        int totalItems, itemLimit, itemsToAdd;
+        
+        //Placeholder Ammounts
+        totalItems = 15;
+        itemLimit = 25;
+        itemsToAdd = 3;
+        
+        //Calculate if sufficiant room for new items
+        if((totalItems + itemsToAdd) <= itemLimit){
+            totalItems = (totalItems + itemsToAdd);
+            System.out.println(totalItems);
+            System.out.println("Added successfully!");
+        }
+        else {
+            System.out.println("You do not have enough room in your backpack.");
+        }
+    }
 }
--- src/byui/cit260/vikingQuest/control/calculateCurrentScore.java
+++ src/byui/cit260/vikingQuest/control/calculateCurrentScore.java
@@ -10,5 +10,19 @@ package byui.cit260.vikingQuest.control;
  * @author Amy
  */
 public class calculateCurrentScore {
-    
+    //Place Holder calculation
+    public static void main(String args[]){
+        
+        //Class variables
+        int lastScore, pointsThisSession, totalPoints;
+        
+        //Place Holder scores
+        lastScore = 1000;
+        pointsThisSession = 50;
+        totalPoints = lastScore + pointsThisSession;
+        
+        //Display current total points
+        System.out.println("Your current Score is: 10050");
+        
+    }
 }
