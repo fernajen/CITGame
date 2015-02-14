b6a3762db2480b5aa3d7cf87e5cca1245ac081af
EstevezAmy <amynboren@gmail.com>
2015-02-14 12:53:01 -0600

    test packages

---
+++ test/byui/cit260/vikingQuest/control/calculateCurrentScoreTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.vikingQuest.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Amy
+ */
+public class calculateCurrentScoreTest {
+    
+    public calculateCurrentScoreTest() {
+    }
+
+    /**
+     * Test of main method, of class calculateCurrentScore.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        calculateCurrentScore.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ test/byui/cit260/vikingQuest/control/calculateSuffcientPayTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.vikingQuest.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Amy
+ */
+public class calculateSuffcientPayTest {
+    
+    public calculateSuffcientPayTest() {
+    }
+
+    /**
+     * Test of main method, of class calculateSuffcientPay.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        calculateSuffcientPay.main();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
