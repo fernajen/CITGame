29dde2422192205934ec4c4c4a5531e8bcc9867c
Jenny <Jenny@192.168.0.114>
2015-02-18 15:37:33 -0500

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/byui/cit/vikingQuest/view/ProgramControl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit.vikingQuest.view;
+
+import byui.cit260.vikingQuest.model.Player;
+
+/**
+ *
+ * @author Jenny
+ */
+class ProgramControl {
+
+    static Player createPlayer(String playersName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
