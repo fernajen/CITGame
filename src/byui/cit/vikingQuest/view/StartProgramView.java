2f6daca1a14f7cda9d21fee1d28cde6de21d1d8a
Jenny <Jenny@192.168.0.114>
2015-02-18 14:43:29 -0500

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/byui/cit/vikingQuest/view/StartProgramView.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit.vikingQuest.view;
+
+/**
+ *
+ * @author Jenny
+ */
+public class StartProgramView {
+    public StartProgramView() {
+        
+    }
+    public void startProgram(){
+        // Display banner screen
+        this.displayBanner();
+ 
+        // Prompt player to enter name and retrive the player name
+        // create and save player object
+        // display a personalized welcome message
+        // display main menu
+
+   
+    }
+
+    public void displayBanner() {
+        System.out.println("\n\n***************************************************");
+        
+        System.out.println("*Welcome, in this game you will hunt for a Viking mound with treasure!*");
+        
+        System.out.println("\n\n****************************************************");
+        
+    }
+        
+    }
+    
