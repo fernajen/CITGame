58a241a526532978eea3c2b55f5072fbb5f35a6d
EstevezAmy <amynboren@gmail.com>
2015-02-27 12:32:01 -0600

    Feb 27 - Amy Game Menu

--- src/byui/cit260/vikingQuest/view/GameMenuView.java
+++ src/byui/cit260/vikingQuest/view/GameMenuView.java
@@ -5,14 +5,141 @@
  */
 package byui.cit260.vikingQuest.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Amy
  */
 public class GameMenuView {
+        
+    //Game Menu
+    private final String GAMEMENU = "\n"
+        + "\n---------------------"
+        + "\n|     Game Menu     |"
+        + "\n---------------------"
+        + "\nV - View Map"
+        + "\nB - Backpack"
+        + "\nT - Talk to someone"
+        + "\nG - Give Item"
+        + "\nI - Item Recieved"
+        + "\nH - Help"
+        + "\nM - Main Menu"
+        + "\n---------------------";
     
-    //Placeholder Function
-    void displayMenu(){
-        System.out.println("\n*** GameMenu.displayMenu stub function called ***");
+    public void displayMenu() {
+
+        
+        char selection = ' ';
+        do {
+            System.out.println(GAMEMENU); //Display the main menu
+            
+            String input = this.getInput(); // Get user selection
+            
+            selection = input.charAt(0);
+        
+            this.doAction(selection);  //Do action selected
+        
+        } while (selection != 'E'); // A selection is not Exit
+        
     }
+
+    private String getInput() {
+        boolean valid = false;// indicates whether name has been retrived.
+        String selection = null;
+        Scanner keyboard = new Scanner(System.in);// keyboard input stream
+        
+        //Ask for and recieve user name input Placeholder
+        System.out.println("\n*** The user input is requested and stored. ***");
+        
+        while(!valid){
+                
+            //prompt user for input
+            System.out.println("Enter the letter of your selection: \n");
+            
+            //get the name from the keyboard trim off blank spaces
+            selection = keyboard.nextLine();
+            selection = selection.trim();
+                
+            //if the input is invalid send out error message
+            if(selection.length()>=2){
+                System.out.println("Invalid entry - Please Select a letter.");
+                continue; // and repeat again
+                   
+            }
+            break; // out of the (exit) the repitition.
+            
+        }
+      
+        return selection;
+    }
+    
+    public void doAction(char choice) {
+        switch (choice) {
+            case 'V': // View Map
+                this.viewMap();
+                break;
+            case 'B': // Backpack
+                this.viewBackpack();
+                break;
+            case 'T': // Talk to someone
+                this.talk();
+                break;
+            case 'G': // Give Item
+                this.giveItem();
+                return;
+            case 'I': // Item (Received)
+                this.itemRecieved();
+                return;
+            case 'H': // Item (Received)
+                this.displayHelpMenu();
+                return;
+            case 'M': // Exit to Main Menu
+                this.returnToMain();
+                return;
+            default:
+                System.out.println("\n*** Invalid Selection *** Try Again");
+                break;
+        }
+    }
+    
+    
+    private void viewMap() {
+        
+        //Placeholder Comment
+        System.out.println("*** viewMap() function is called ***");
+        
+    }
+    
+    private void viewBackpack() {
+        
+        //Placeholder mark begining of function call
+        System.out.println("*** viewBackpack() function called ***");
+        
+    }
+    
+    private void talk() {
+        
+        System.out.println("*** talk() function called ***");
+    }
+    
+    private void giveItem() {
+        
+        System.out.println("*** giveItem()displayHelpMenu function called ***");
+    } 
+    
+    private void itemRecieved() {
+        
+        System.out.println("*** itemRecieved()displayHelpMenu function called ***");
+    } 
+    
+    private void displayHelpMenu(){
+        
+        System.out.println("*** displayHelpMenu() function called ***");
+    } 
+    
+    private void returnToMain() {
+        
+        System.out.println("*** returnToMainMenu() function called ***");
+    } 
 }
