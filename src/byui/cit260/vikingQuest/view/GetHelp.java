24e6e8ba31972df7e1294929eeed17e0f6f20a67
EstevezAmy <amynboren@gmail.com>
2015-03-07 22:44:20 -0600

    Saving as I go - Amy

--- src/byui/cit260/vikingQuest/view/GetHelp.java
+++ src/byui/cit260/vikingQuest/view/GetHelp.java
@@ -5,14 +5,13 @@
  */
 package byui.cit260.vikingQuest.view;
 import byui.cit260.vikingQuest.control.GameControl;
-import java.util.Scanner;
 import vikingquest.VikingQuest;
 
 /**
  *
  * @author Amy
  */
-public class GetHelp {
+public class GetHelp extends View{
     private final String MENU = "\n"
             + "\n-------------------------"
             + "\n|        Help Menu      |"
@@ -22,102 +21,12 @@ public class GetHelp {
             + "\nH - How to use your items"
             + "\nQ - Quit Help"
             + "\n---------------------------";
-    public GetHelp(){
-        char selection = ' ';
-        do {
-            System.out.println(MENU); //display help menu
-            
-            String input = this.getInput(); // get input
-            
-            selection = input.charAt(0); //first line of character
-            this.doAction(selection); // action decided based on selection
 
-        }
-        while(selection != 'Q'); 
+    public GetHelp(String promptMessage) {
+        super(promptMessage);
     }
-        public abstract class View implements ViewInterface{
-       
-        
-        private String promptMessage;
 
-        public String getPromptMessage() {
-            return promptMessage;
-        }
-
-        public void setPromptMessage(String promptMessage) {
-            this.promptMessage = promptMessage;
-        }
-        @Override
-    public String getInput(){
-    Scanner keyboard = new  Scanner(System.in);
-    boolean valid = false;
-    String selection = null;
-    // while a valid name has not been retrived.
-    while(!valid){
-        //get value from the keyboard
-        selection = keyboard.nextLine();
-        selection = keyboard.trim();
-        
-        if (selection.length() < 1){ //blank value entered
-            System.out.println("\n*** Invalid selection *** Try again");
-            continue;
-        }
-        break;
-    }
-    return selection;
-}
-    @Override
-        public String getInput(){
-            Scanner keyboard = new Scanner(System.in);
-            boolean valid = false;
-            String selection = null;
-            //while a valid name hasn't been inputed
-            while(!valid){
-                //prompt for player's name
-                System.out.println("\t\Enter your selection below");
-                //get the value entered via keyboard
-                selection = keyboard.nextLine();
-                selection = selection.trim();
-                
-                if (selection.length()<1){//blank value entered
-                    System.out.println("\n*** Invalid selection ***try again")
-                            continue;
-                }
-                break;
-            }
-            return selection;
-        }
-        public void doAction(){
-            switch(choice){
-                case 'G':
-                    this.learnGoal();
-                    break;
-                case 'M':
-                    this.learnMovement();
-                    break;
-                case 'H':
-                    this.learnItems();
-                    break;
-                case 'Q':
-                    return;
-                    default:
-                System.out.println("\n*** Invalid Selection *** Try Again");
-                break;
-            }
-        }
-
-        private void learnGoal() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        private void learnMovement() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        private void learnItems() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
+    
     public void getHelp(char choice){
         switch(choice){ 
                case 'G': // This will teach you the goal of the game
@@ -158,13 +67,10 @@ public class GetHelp {
         while (selection != 'Q'); // A selection is not Exit
     }
 
-    private String getInput() {
+    @Override
+    public void doAction(String value) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-
-}
-
 
 
 }
