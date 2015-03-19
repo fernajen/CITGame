6f1bdd7451bb82126cfb0bb054d284475dc1769c
EstevezAmy <EstevezAmy@users.noreply.github.com>
2015-03-19 16:50:29 -0500

    Update startNewGame.java

--- src/byui/cit260/vikingQuest/control/startNewGame.java
+++ src/byui/cit260/vikingQuest/control/startNewGame.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package byui.cit260.vikingQuest.control;
+
+import byui.cit260.vikingQuest.model.Player;
 
 /**
  *
@@ -11,4 +9,7 @@ package byui.cit260.vikingQuest.control;
  */
 public class startNewGame {
     
+    public static void startNewGame(Player player){
+        System.out.println("*** startNewGame function called ***");
+    }
 }
