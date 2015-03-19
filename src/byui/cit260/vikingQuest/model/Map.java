6a1f82fcd67b7c350618a27c243ca465cdc7c007
EstevezAmy <EstevezAmy@users.noreply.github.com>
2015-03-19 17:00:00 -0500

    Update Map.java

--- src/byui/cit260/vikingQuest/model/Map.java
+++ src/byui/cit260/vikingQuest/model/Map.java
@@ -4,51 +4,68 @@
  * and open the template in the editor.
  */
 package byui.cit260.vikingQuest.model;
+import byui.cit260.vikingQuest.control.GameControl;
 import java.io.Serializable;
-import java.util.Objects;
 /**
  *
  * @author Amy
  */
 public class Map implements Serializable{
+
+    private static Scene[] createScenes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     // Instance Class Variables
-    private String entireMap;
-    private String locationsInMap;
+    private int noOfRows;
+    private int noOfColums;
+    private Location[][] locations;
     
+    public Map(){
+    }
     
-    // ToString, equals, hashCode
-    @Override
-    public String toString() {
-        return "Map{" + "entireMap=" + entireMap + ", locationsInMap=" + locationsInMap + '}';
+    public Map(int noOfRows, int noOfColumns){
+        
+        if (noOfRows < 1 || noOfColumns < 1){
+            System.out.println("The number of rows and columns must be > zero");
+            return;
+        }
+        
+        this.noOfRows = noOfRows;
+        this.noOfColums = noOfColumns;
+        
+        // Create 2-D array for location objects
+        this.locations = new Location[noOfRows][noOfColumns];
+        
+        for (int row = 0; row < noOfRows; row++){
+            for(int colum = 0; colum < noOfColumns; colum++){
+                // Create and initalize new location object instance
+                Location location = new Location();
+                location.setColumn(colum);
+                location.setRow(row);
+                location.setVisited(false);
+                
+                // Assign the location to the current position in array
+                locations[row][colum] = location;
+            }
+        }
+    }
+    
+    private static Map createMap(){
+        // Create the map
+        Map map = new Map(20,20);
+        
+        // Create scenes for the game
+        Scene[] scenes = createScenes();
+        
+        // Assignscenes to locations
+        GameControl.assignScenesToLocations(map, scenes);
+        
+        return map;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 89 * hash + Objects.hashCode(this.entireMap);
-        hash = 89 * hash + Objects.hashCode(this.locationsInMap);
-        return hash;
+    public Location[][] getLocations() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Map other = (Map) obj;
-        if (!Objects.equals(this.entireMap, other.entireMap)) {
-            return false;
-        }
-        if (!Objects.equals(this.locationsInMap, other.locationsInMap)) {
-            return false;
-        }
-        return true;
-    }
-    
-    
     
 }
